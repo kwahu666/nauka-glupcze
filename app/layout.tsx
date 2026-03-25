271913046dd7e1511f9d4912feda5af25d0e199c
kWaHu666 <142489248+kwahu666@users.noreply.github.com>
2026-03-25 13:35:38 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,12 +4,23 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Nauka, Głupcze! - Najlepszy agregator naukowy",
   description: "Wiadomości ze świata nauki, kosmosu i technologii w jednym miejscu. Bądź na bieżąco – czytaj artykuły i oglądaj najnowsze materiały wideo.",
-  metadataBase: new URL("https://naukaglupcze.pl"),
+  // Dodane WWW do bazy
+  metadataBase: new URL("https://www.naukaglupcze.pl"),
   openGraph: {
     title: "Nauka, Głupcze! - Agregator Naukowy",
     description: "Wiadomości ze świata nauki, kosmosu i technologii w jednym miejscu. Bądź na bieżąco z najnowszymi odkryciami.",
-    url: "https://naukaglupcze.pl",
+    // Dodane WWW do urla
+    url: "https://www.naukaglupcze.pl",
     siteName: "Nauka, Głupcze!",
+    images: [
+      {
+        // Twardy, pełny adres do pliku z obrazkiem i WWW
+        url: "https://www.naukaglupcze.pl/og-image.jpg",
+        width: 1200,
+        height: 630,
+        alt: "Nauka Głupcze",
+      },
+    ],
     locale: "pl_PL",
     type: "website",
   },
@@ -17,6 +28,7 @@ export const metadata: Metadata = {
     card: "summary_large_image",
     title: "Nauka, Głupcze! - Agregator Naukowy",
     description: "Najświeższe wiadomości ze świata nauki i kosmosu.",
+    images: ["https://www.naukaglupcze.pl/og-image.jpg"],
   },
   icons: {
     icon: "/logo.svg", 
